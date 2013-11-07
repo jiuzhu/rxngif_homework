a0227217d8ad7214bce209f74bea4907c7b26b30
Julia Wang <jiuzhu@gmail.com>
2013-11-07 15:56:25 -0500

    Week 6 Homework

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,37 @@
 class PicturesController < ApplicationController
   def show
+	@picture = Picture.find(params[:id])
   end
+
+  def index
+	@picture = Picture.all
+  end
+
+  def new
+  end
+
+  def create
+	p = Picture.new
+	p.source = params[:source]
+	p.caption = params[:caption]
+	p.save
+  end
+
+  def destroy
+	p = Picture.find(params[:id])
+	p.destroy
+  end
+
+  def edit
+  @picture = Picture.find(params[:id])
+  end
+
+  def update
+  p = Picture.find(params[:id])
+  p.caption = params[:caption]
+  p.source = params[:source]
+  p.save
+  end
+
+
 end
